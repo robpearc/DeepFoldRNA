d090f897236d4e21c9eedead7b154a91a596afe4
Robin Pearce <robpearc@umich.edu>
2022-05-31 14:25:58 -0400

    Update embedders.py

--- network/embedders.py
+++ network/embedders.py
@@ -4,7 +4,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-
+import numpy as np
 
 class Input_Embedder(nn.Module):
     def __init__(self,config,input_config,device):
